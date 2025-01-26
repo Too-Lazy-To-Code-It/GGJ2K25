cee2628eb71a02a3a6afc2fb02c34237a2dbbc6f
Yassine <100700676+Enissayleaae@users.noreply.github.com>
2025-01-26 06:12:46 +0100

    Fix High

--- Assets/Code/Scripts/Characters/Bubble/BubbleLocomotion.cs
+++ Assets/Code/Scripts/Characters/Bubble/BubbleLocomotion.cs
@@ -218,7 +218,7 @@ namespace Code.Scripts.Characters.Bubble
                 _bubbleManager.rb.linearDamping = 0; 
             
 
-            if (collision.gameObject.CompareTag("Ground"))
+            if (collision.gameObject.CompareTag("obstacle"))
             {
                 
                 Vector3 currentVelocity = _bubbleManager.rb.linearVelocity;
